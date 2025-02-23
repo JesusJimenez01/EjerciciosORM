6945633ae7efbfd7e0470f3d94a06ffbef30897a
JesusJimenez01 <jjimper591@g.educaand.es>
2025-02-24 00:55:25 +0100

    Volver a sqlite

--- sistema_gestion_restaurantes/settings.py
+++ sistema_gestion_restaurantes/settings.py
@@ -77,14 +77,10 @@ WSGI_APPLICATION = 'sistema_gestion_restaurantes.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': 'django_db',
-       'USER': 'postgres',
-       'PASSWORD': 'password',
-       'HOST': 'db',
-       'PORT': '5432',
-   }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
